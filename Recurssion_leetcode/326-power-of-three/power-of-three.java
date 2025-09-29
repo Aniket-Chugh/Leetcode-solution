88d4c2bf661dd816a8d3b6d39aad552f594b3fc0
Aniket Chugh <149312276+Aniket-Chugh@users.noreply.github.com>
2025-09-29 15:02:13 +0530

    Time: 7 ms (100.00%) | Memory: 43.7 MB (99.26%) - LeetSync

---
+++ Recurssion_leetcode/326-power-of-three/power-of-three.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+        if(n%3 != 0) return false;
+      boolean result =  isPowerOfThree(n/3);
+      return result;
+    }
+}

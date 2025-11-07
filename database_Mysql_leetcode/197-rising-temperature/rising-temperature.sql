cdcdec2e105af0c064c2dd7ff75e44989d7cb2a4
Aniket Chugh <149312276+Aniket-Chugh@users.noreply.github.com>
2025-11-07 09:50:15 +0530

    Time: 439 ms (71.95%) | Memory: 0B (100.00%) - LeetSync

--- database_Mysql_leetcode/197-rising-temperature/rising-temperature.sql
+++ database_Mysql_leetcode/197-rising-temperature/rising-temperature.sql
@@ -1,7 +1,10 @@
-SELECT id
-FROM Weather w1
-WHERE temperature > (
-    SELECT temperature
-    FROM Weather w2
-    WHERE w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
-);
+SELECT 
+    w1.id
+FROM 
+    Weather w1
+JOIN 
+    Weather w2
+ON 
+    DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE 
+    w1.temperature > w2.temperature;

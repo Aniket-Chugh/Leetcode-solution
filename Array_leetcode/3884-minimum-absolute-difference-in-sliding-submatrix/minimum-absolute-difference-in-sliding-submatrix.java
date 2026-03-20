ea58a5f06380efbfa7e97b80673de645b131c146
Aniket Chugh <149312276+Aniket-Chugh@users.noreply.github.com>
2026-03-20 21:58:03 +0530

    Time: 26 ms (48.94%) | Memory: 47.7 MB (40.43%) - LeetSync

---
+++ Array_leetcode/3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int[][] minAbsDiff(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length;
+        int[][] ans = new int[m - k + 1][n - k + 1];
+
+        for (int i = 0; i <= m - k; i++) {
+            for (int j = 0; j <= n - k; j++) {
+
+                List<Integer> list = new ArrayList<>();
+
+                for (int x = i; x < i + k; x++) {
+                    for (int y = j; y < j + k; y++) {
+                        list.add(grid[x][y]);
+                    }
+                }
+
+                Collections.sort(list);
+
+                int minDiff = Integer.MAX_VALUE;
+
+                for (int t = 1; t < list.size(); t++) {
+                    // 🔥 skip same values
+                    if (list.get(t).equals(list.get(t - 1))) continue;
+
+                    minDiff = Math.min(minDiff, list.get(t) - list.get(t - 1));
+                }
+
+                // 🔥 if all values same → no valid pair
+                if (minDiff == Integer.MAX_VALUE) {
+                    ans[i][j] = 0;
+                } else {
+                    ans[i][j] = minDiff;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
